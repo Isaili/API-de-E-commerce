fd03cccc640abbc66b05c8b625796fd69c5c07a4
isaili <231173@ids.upchiapas.edu.mx>
2026-04-23 11:26:38 -0600

    entities-user

---
+++ ecommerce-api/src/users/entities/user.entity.ts
@@ -0,0 +1,56 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+  OneToOne,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { UserRole } from '../../common/enums/user-role.enum';
+import { Order } from '../../orders/entities/order.entity';
+import { Cart } from '../../cart/entities/cart.entity';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  @Exclude() 
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.CUSTOMER })
+  role: UserRole;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: true })
+  address: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @OneToMany(() => Order, (order) => order.user)
+  orders: Order[];
+
+  @OneToOne(() => Cart, (cart) => cart.user)
+  cart: Cart;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
